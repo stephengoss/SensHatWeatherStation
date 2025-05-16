d53c1f888fdfde3a47ad9f12a4a84cdef0b7f742
Stephen Goss <stephengoss@gmx.net>
2025-05-16 21:05:13 +0200

    Update weather_station.py
    Clean up

--- pi_weather_station/weather_station.py
+++ pi_weather_station/weather_station.py
@@ -174,7 +174,6 @@ def rethinkBrightness(brightness):
     e = [0, 0, 0]   # empty
     g = [0, 55+brightness, 0]  # green
 
-
 if not device_folder_check:
     print("No DS18B20 sensor found. Continuing with SENSHAT data.")
     temp_sensor = "SENSHAT"
