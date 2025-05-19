29efaed0531130818aae4a016beb32673ca919fa
stephengoss <stephengoss@gmx.net>
2025-05-19 12:57:04 +0200

    Cleanup Ignore

--- pi_weather_station/config.py
+++ pi_weather_station/config.py
@@ -4,5 +4,5 @@ class Config:
     STATION_KEY = "XeshVHi1"
     SCREEN_ROTATION = 0
     WEATHER_UPLOAD = False
-    DISPLAY_SYMBOL = False
-    DISPLAY_TEXT = False
+    DISPLAY_SYMBOL = True
+    DISPLAY_TEXT = True
